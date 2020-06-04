706a97726f789335d4201ff9b13d96928f419f2b
Cristian Morataya <cris.morataya@gmail.com>
2020-06-04 11:21:55 -0600

    Updates

---
+++ precache-manifest.86a29c6b2929e715b5a86b40545976cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0dfc17418fed7c68166",
+    "url": "/lemonade/static/css/main.8e953878.chunk.css"
+  },
+  {
+    "revision": "d0dfc17418fed7c68166",
+    "url": "/lemonade/static/js/main.7f6f6852.chunk.js"
+  },
+  {
+    "revision": "7a84b9a983904db16435",
+    "url": "/lemonade/static/js/runtime~main.483f0ea9.js"
+  },
+  {
+    "revision": "8120163d6d3b91971b22",
+    "url": "/lemonade/static/js/2.c0796c35.chunk.js"
+  },
+  {
+    "revision": "78724131f01d3b41752dff2a63cab63c",
+    "url": "/lemonade/index.html"
+  }
+];
